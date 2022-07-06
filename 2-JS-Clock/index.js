d3eb4e7e3f652b42f1cdc61a45e9583370426c56
Jake Samuels <samuels242@googlemail.com>
2022-07-06 11:48:20 +0100

    CSS + JS Clock

--- 2-JS-Clock/index.js
+++ 2-JS-Clock/index.js
@@ -5,16 +5,16 @@ const hourHand = document.querySelector(".hour-hand");
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
-  const secondsDegrees = (seconds / 60) * 360 + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const secondDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
   const mins = now.getMinutes();
-  const minsDegrees = (mins / 60) * 360 + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  const minDegrees = (mins / 60) * 360 + 90;
+  minsHand.style.transform = `rotate(${minDegrees}deg)`;
 
   const hours = now.getHours();
-  const hoursDegrees = (hours / 12) * 360 + (mins / 60) * 30 + 90;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  const hourDegrees = (hours / 12) * 360 + (mins / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
